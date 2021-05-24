4abeb10da8a28966e7e2a5dec0769457a906a5db
Natalia Martín <nmzaballos@gmail.com>
2021-05-24 13:04:31 +0200

    painting favourites in progress

--- main.js
+++ main.js
@@ -3,22 +3,20 @@
 const input = document.querySelector(".input");
 const button = document.querySelector(".button");
 const list = document.querySelector(".list");
-//VARIABLE DE INTENTO 1 Y 2:
-//let seriesList = [];
-
 let arrayDoble = [];
-const arrayGuay = [];
+let favourites = [];
+// if(localStorage.getItem("arrayDoble") === null) {
+// getList(); pero tengo el problema de que el inputValue depende de un evento q se produce más adelante!!
+// }
 
 function getList() {
     const inputValue = input.value;
-    
-    fetch(`http://api.tvmaze.com/search/shows?q=${inputValue}`)
+    fetch(`//api.tvmaze.com/search/shows?q=${inputValue}`)
     .then(response => response.json())
     .then((data) => {
          arrayDoble = new Array(data.length);
         for(let i = 0; i < data.length; i++) {
             arrayDoble[i] = new Array(3);
-           // arrayDoble[i][0] = new Array(2); posiblemente me sobre
         }
         for(let i = 0; i < data.length; i++) {
             arrayDoble[i][0] = data[i].show.image;
@@ -26,35 +24,62 @@ function getList() {
             arrayDoble[i][1] = data[i].show.name;
             arrayDoble[i][2] = data[i].show.id;
         }
+        localStorage.setItem("arrayDoble", JSON.stringify(arrayDoble));
         paintList(arrayDoble); //FUNCIONA!!!!!
       }
     );
 }
-
 function paintList(arrayDoble){
     let seriesList = "";
   //  console.log(arrayDoble[5][0].medium); ¡¡funciona!!
     for (let i = 0;  i < arrayDoble.length; i++) {
         const image = arrayDoble[i][0];        
         const titleSeries= arrayDoble[i][1];
-        const identifyer = arrayDoble[i][2];
+        const identifying = arrayDoble[i][2];
         const placeHolderRef = "https://via.placeholder.com/100x150/ffffff/666666/?text=TV";
         if(image === null) {
-            seriesList = `<li id=${identifyer}><img src="${placeHolderRef}" alt="sin cartel">${titleSeries}</li>`;
+            seriesList = `<li class="js-card" data-id="${identifying}"><div class="card"><img class="image" src="${placeHolderRef}" alt="sin cartel">${titleSeries}</div></li>`;
         }
         else {
-            seriesList = `<li id=${identifyer}><img src="${image.medium}" alt="cartel">${titleSeries}</li>`;
+            seriesList = `<li class="js-card" data-id="${identifying}"><div class="card"><img class="image" src="${image.medium}" alt="cartel">${titleSeries}</div></li>`;
         }
         list.innerHTML += seriesList;
-
    }
-
+   addListenersToCards();
+   //Pintar las favoritas: buscar si el elmento que se está pintando está en el array de favoritas. Sustituyo el cardID por la referencia a ese elemento q tengo dentro de esta función, q es identifying:
+   const isInside = favourites.find(idFavourite => idFavourite === identifying);
 }
-
 function handleSearch(event) {
     event.preventDefault();
     getList();
 }
-
-
 button.addEventListener("click", handleSearch);
+
+//FAVORITAS:
+
+function addListenersToCards() {
+    const allCards = document.querySelectorAll(".js-card");
+    for(const card of allCards) {
+        card.addEventListener("click", handleClickCard);
+    }
+}
+function handleClickCard(event) {
+   // const whereTheUserClicked = event.target; //no hace falta
+    //PASO 1: identifica la li pulsada:
+    const whereIAddedTheEvent = event.currentTarget; 
+    //PASO 2: obtener la info de esa card:
+    const cardId = whereIAddedTheEvent.dataset.id;
+    //console.log(cardId) funciona!!
+    //PASO 3: buscar si el elmento por el que identifico la card está en el array de favoritas:
+    const isInside = favourites.find(idFavourite => idFavourite === cardId);
+    //PASO 4: guardarla en o quitarla del array de favoritas, guardando o quitando el elemento identificador:
+    if(isInside === undefined) {
+        favourites.push(cardId);
+    }
+    else {
+        favourites = favourites.filter(idFavourite !== cardId);
+    }
+    // console.log(favourites); funciona!! 
+    //PASO 5: pintar las favoritas:
+    //paintList();
+}
